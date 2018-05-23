7940936acc32469eee68c34b4cfecbd104db82ab
Zachary Wu <1123982873@qq.com>
2018-05-24 00:34:43 +0800

    2.4返回栈和活动的生命周期

---
+++ app/src/main/java/com/example/zacharywu/exploreactivity/activities/NormalActivity.java
@@ -0,0 +1,15 @@
+package com.example.zacharywu.exploreactivity.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zacharywu.exploreactivity.R;
+
+public class NormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal);
+    }
+}
